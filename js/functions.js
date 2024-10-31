d6bfd2fcf73ca2eb2b121694a284f4c696057365
BlackYoshiii <alexfrol219@gmail.com>
2024-10-31 20:13:50 +0700

    Создаёт функцию о возможности проведения встречи

--- js/functions.js
+++ js/functions.js
@@ -31,3 +31,27 @@ const extractNumbers = (str) => {
 };
 
 extractNumbers('Коля 230 и -1 годик');
+
+const getMinutes = function (time) {
+
+  const arrayTime = time.split(':');
+  const sumMinutes = Number(arrayTime[0]) * 60 + Number(arrayTime[1]);
+
+  return sumMinutes;
+};
+
+const getTrueTime = function (startWorkDay, endWorkDay, startMeeting, duration) {
+  const startMeetinMinutes = getMinutes(startMeeting);
+
+  if (getMinutes(startWorkDay) > startMeetinMinutes || startMeetinMinutes + duration > getMinutes(endWorkDay)) {
+    return false;
+  }
+
+  return true;
+};
+
+console.log(getTrueTime('08:00', '17:30', '14:00', 90)); // true
+console.log(getTrueTime('8:0', '10:0', '8:0', 120)); // true
+console.log(getTrueTime('08:00', '14:30', '14:00', 90)); // false
+console.log(getTrueTime('14:00', '17:30', '08:0', 90)); // false
+console.log(getTrueTime('8:00', '17:30', '08:00', 900)); // false
