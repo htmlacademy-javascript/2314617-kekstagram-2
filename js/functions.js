f4d21cc57fa9c43f399d58df1f2d15d2eaf2808f
BlackYoshiii <alexfrol219@gmail.com>
2024-10-31 20:16:10 +0700

    Убирает команды консоли

--- js/functions.js
+++ js/functions.js
@@ -50,8 +50,8 @@ const getTrueTime = function (startWorkDay, endWorkDay, startMeeting, duration) 
   return true;
 };
 
-console.log(getTrueTime('08:00', '17:30', '14:00', 90)); // true
-console.log(getTrueTime('8:0', '10:0', '8:0', 120)); // true
-console.log(getTrueTime('08:00', '14:30', '14:00', 90)); // false
-console.log(getTrueTime('14:00', '17:30', '08:0', 90)); // false
-console.log(getTrueTime('8:00', '17:30', '08:00', 900)); // false
+getTrueTime('08:00', '17:30', '14:00', 90); // true
+getTrueTime('8:0', '10:0', '8:0', 120); // true
+getTrueTime('08:00', '14:30', '14:00', 90); // false
+getTrueTime('14:00', '17:30', '08:0', 90); // false
+getTrueTime('8:00', '17:30', '08:00', 900); // false
