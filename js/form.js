9ecea8141e4a202af6ff8ae3ee3a6bb8159f306a
BlackYoshiii <alexfrol219@gmail.com>
2025-01-31 17:37:35 +0700

    Исправляет код для валидации и закрытия формы

--- js/form.js
+++ js/form.js
@@ -1,3 +1,5 @@
+const MAX_COMMENTS_LENGTH = 140;
+
 const bodyElement = document.body;
 const uploadInput = document.querySelector('.img-upload__input');
 const uploadOverlay = document.querySelector('.img-upload__overlay');
@@ -5,6 +7,12 @@ const uploadCancel = document.querySelector('.img-upload__cancel');
 const uploadForm = document.querySelector('.img-upload__form');
 const textHashtags = document.querySelector('.text__hashtags');
 const textDescription = document.querySelector('.text__description');
+
+const formFields = [textHashtags, textDescription].filter((field) => field !== null);
+
+const isFormFieldActive = () => formFields.some((field) => field === document.activeElement);
+
+const checkKeydown = (key) => key === 'Escape';
 
 const closeUploadForm = () => {
   uploadOverlay.classList.add('hidden');
@@ -16,7 +24,7 @@ const onUploadFormClick = () => {
 };
 
 const onUploadFormKeydown = (evt) => {
-  if (evt.key === 'Escape') {
+  if (checkKeydown(evt.key) && !isFormFieldActive()) {
     closeUploadForm();
   }
 };
@@ -31,23 +39,18 @@ uploadInput.addEventListener('change', (evt) => {
   document.addEventListener('keydown', onUploadFormKeydown);
 });
 
-textHashtags.addEventListener('keydown', (evt) => {
-  if (evt.key === 'Escape') {
-    evt.stopPropagation();
-  }
-});
-
-textDescription.addEventListener('keydown', (evt) => {
-  if (evt.key === 'Escape') {
-    evt.stopPropagation();
-  }
-});
-
 const pristineForm = new Pristine(uploadForm, {
   classTo: 'img-upload__field-wrapper',
   errorTextParent: 'img-upload__field-wrapper',
   errorTextClass:'.img-upload__field-wrapper--error',
 });
+
+pristineForm.addValidator(textDescription, (value) => {
+  if (value.length > MAX_COMMENTS_LENGTH) {
+    return false;
+  }
+  return true;
+}, 'Максимальная длина 140 символов', 1, true);
 
 pristineForm.addValidator(textHashtags, (value) => {
   const hashtagsCountRegExp = /^(#[a-zа-яё0-9]{1,19}(\s+|\s*$)){1,5}$/i;
@@ -62,15 +65,17 @@ pristineForm.addValidator(textHashtags, (value) => {
 }, 'Введён невалидный хэштег', 3, true);
 
 pristineForm.addValidator(textHashtags, (value) => {
-  const hashtags = value.split(' ').filter((valueFilter) => valueFilter !== '');
+  const hashtags = value
+    .toLowerCase()
+    .split(' ')
+    .filter((tag) => tag.trim() !== '');
 
-  for (let i = 0; i <= hashtags.length; i++) {
-    for (let j = i + 1; j <= hashtags.length; j++) {
-      if (hashtags[i] === hashtags[j]) {
-        return false;
-      }
-    }
+  const uniqueHastags = new Set(hashtags);
+
+  if (uniqueHastags.size !== hashtags.length) {
+    return false;
   }
+
   return true;
 }, 'Хэштеги повторяются', 1, true);
 
